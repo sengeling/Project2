66d14d458a5d39fe6ce145aac9bb46ee4f7c2641
wgoal <willygoal@gmail.com>
2019-05-23 16:35:58 -0500

    Add sample routes to app.py, branching/merge test

--- app.py
+++ app.py
@@ -23,3 +23,38 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db/bigfoot.sqlite"
 
 db = SQLAlchemy(app)
+
+@app.route("/")
+def home():
+    return "Welcome!"
+
+
+@app.route("/send", methods=["GET", "POST"])
+def send():
+    if request.method == "POST":
+        nickname = request.form["nickname"]
+        age = request.form["age"]
+
+        pet = Pet(nickname=nickname, age=age)
+        db.session.add(pet)
+        db.session.commit()
+
+        return "Thanks for the form data!"
+
+    return render_template("form.html")
+
+
+@app.route("/api/data")
+def list_pets():
+    results = db.session.query(Pet.nickname, Pet.age).all()
+
+    pets = []
+    for result in results:
+        pets.append({
+            "nickname": result[0],
+            "age": result[1]
+        })
+    return jsonify(pets)
+
+if __name__ == "__main__":
+    app.run()
