83187a74fed9f07e33c9dfbf50c358884a90feb7
Ashley McGee <6312276+AshleyMcGee@users.noreply.github.com>
2019-05-25 15:21:30 -0500

    Update app.py in branch to match master

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 # import necessary libraries
 import os
+
+import pandas as pd
+import numpy as np
+
 from flask import (
     Flask,
     render_template,
     jsonify,
     request,
     redirect)
-from sqlalchemy import func
-import pandas as pd
 from flask_sqlalchemy import SQLAlchemy
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
 
 #################################################
 # Flask Setup
@@ -20,41 +27,52 @@ app = Flask(__name__)
 #################################################
 
 # Need to change "data" to whatever the name
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db/data.sqlite"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db/data.sqlite"
+#db = SQLAlchemy(app)
 
-db = SQLAlchemy(app)
+# reflect an existing database into a new model
+#Base = automap_base()
+## reflect the tables
+#Base.prepare(db.engine, reflect=True)
+
+# Save references to each table
+#Samples_Metadata = Base.classes.sample_metadata
+#Samples = Base.classes.samples
+
+
 
 @app.route("/")
-def home():
-    return "Welcome!"
+def index():
+    """Return the homepage."""
+    return render_template("index.html")
 
 
-@app.route("/send", methods=["GET", "POST"])
-def send():
-    if request.method == "POST":
-        nickname = request.form["nickname"]
-        age = request.form["age"]
+#@app.route("/send", methods=["GET", "POST"])
+#def send():
+#    if request.method == "POST":
+#        nickname = request.form["nickname"]
+#        age = request.form["age"]
 
-        pet = Pet(nickname=nickname, age=age)
-        db.session.add(pet)
-        db.session.commit()
+#        pet = Pet(nickname=nickname, age=age)
+#        db.session.add(pet)
+#        db.session.commit()
 
-        return "Thanks for the form data!"
+#        return "Thanks for the form data!"
 
-    return render_template("form.html")
+#    return render_template("form.html")
 
 
-@app.route("/api/data")
-def list_pets():
-    results = db.session.query(Pet.nickname, Pet.age).all()
+#@app.route("/api/data")
+#def list_pets():
+#    results = db.session.query(Pet.nickname, Pet.age).all()
 
-    pets = []
-    for result in results:
-        pets.append({
-            "nickname": result[0],
-            "age": result[1]
-        })
-    return jsonify(pets)
+#    pets = []
+#    for result in results:
+#        pets.append({
+#            "nickname": result[0],
+#            "age": result[1]
+#        })
+#    return jsonify(pets)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
