fcf2f8e885fb17bd5a0a0a279fa06e978de51afa
Rocky Racoon <mdoig5247@gmail.com>
2019-06-03 15:15:21 -0500

    small map adjustments

--- static/js/logic.js
+++ static/js/logic.js
@@ -6,25 +6,23 @@
     let stateBoundariesData = await d3.json('../static/js/state_boundaries.json');
     stateBoundariesData = stateBoundariesData.features
 
-    // const countyBoundariesData = await d3.json('county_boundaries.json')
-
     let stateRatesData = await d3.csv('../static/js/states_rates.csv')
 
     // Transform prescription rate/death rate data into an object
     const stateRateReduce = stateRatesData.reduce((prevData, currentData) => {
-        const state = currentData.state;
-        currentData.mean_prescribing_rate = +currentData.mean_prescribing_rate;
-        currentData.mean_opioids_death_rate = +currentData.mean_opioids_death_rate;
-        prevData[state.toLowerCase()] = currentData;
+        const state = currentData.state
+        currentData.mean_prescribing_rate = +currentData.mean_prescribing_rate
+        currentData.mean_opioids_death_rate = +currentData.mean_opioids_death_rate
+        prevData[state.toLowerCase()] = currentData
         return prevData
     }, {})
 
     // Add rates to the GeoJSON properties using Object.assign()
     let states = stateBoundariesData.map(stateBoundary => {
-        const state = stateBoundary.properties.NAME.toLowerCase();
+        const state = stateBoundary.properties.NAME.toLowerCase()
         const property = stateRateReduce[state]
-        stateBoundary.properties = Object.assign({}, stateBoundary.properties, property);
-        return stateBoundary;
+        stateBoundary.properties = Object.assign({}, stateBoundary.properties, property)
+        return stateBoundary
     })
 
     // Create function to assign color based on prescription rate
@@ -39,12 +37,12 @@
 
     // Create function to assign color based on death rate
     function deathGetColor(r) {
-        return r > 10 ? '#252525':
-                r > 8 && r <= 10 ? '#636363':
-                r > 6 && r <= 8 ? '#969696':
-                r > 4 && r <= 6 ? '#bdbdbd':
-                r > 2 && r <= 4 ? '#d9d9d9':
-                                    '#f7f7f7'
+        return r > 10 ? '#006d2c':
+                r > 8 && r <= 10 ? '#31a354':
+                r > 6 && r <= 8 ? '#74c476':
+                r > 4 && r <= 6 ? '#a1d99b':
+                r > 2 && r <= 4 ? '#c7e9c0':
+                                    '#edf8e9'
     }
 
     // Set style for prescription rate layer
@@ -54,7 +52,7 @@
             weight: 1.5,
             color: 'white',
             fillOpacity: 1
-        };
+        }
     }
 
     // Set style for death rate layer
@@ -64,7 +62,7 @@
             weight: 1.5,
             color: 'white',
             fillOpacity: 0.7
-        };
+        }
     }
 
     // Define layers here so they're accessible to the event listeners
@@ -93,21 +91,15 @@
         stateDeathRates.resetStyle(e.target)
     }
 
-    // Event listener zooms closer on state
-    function zoomToFeature(e) {
-        map.fitBounds(e.target.getBounds())
-    }
-
     // Assign events to event listener functions when adding to layers
     function prescriptionOnEachFeature(feature, layer) {
         layer.on({
             mouseover: highlightFeature,
             mouseout: prescriptionResetHighlight,
-            click: zoomToFeature
         })
 
         // Add tooltip for each state
-        layer.bindTooltip(`<h3>${feature.properties.state}</h3>
+        layer.bindTooltip(`<h5>${feature.properties.state}</h5>
             <p><strong>Mean prescribing rate:</strong> ${feature.properties.mean_prescribing_rate}<br>
             <strong>Mean death rate:</strong> ${feature.properties.mean_opioids_death_rate}</p>`
             )
@@ -117,7 +109,6 @@
         layer.on({
             mouseover: highlightFeature,
             mouseout: deathResetHighlight,
-            click: zoomToFeature
         })
 
         // Add tooltip for each state
@@ -138,11 +129,7 @@
         style: deathStyle
     })
 
-    // const countyBoundaries = L.geoJSON(countyBoundariesData, {
-        // pointToLayer: pointToLayer
-    // })
-
-    // Define map layer
+    // Define map layers
     const lightMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         maxZoom: 18,
@@ -150,9 +137,17 @@
         accessToken: API_KEY
     })
 
+    const streetsMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.streets",
+        accessToken: API_KEY
+    })
+
     // Define baseMaps and overlayMaps objects to hold base and overlay layers
     const baseMaps = {
         'Base map' : lightMap,
+        // 'Alternative base' : streetsMap
     }
 
     const overlayMaps = {
@@ -170,7 +165,8 @@
 
     // Create layer control and add to map
     L.control.layers(baseMaps, overlayMaps, {
-        collapsed: false
+        collapsed: false,
+        hideSingleBase: true
     }).addTo(map)
 
     // Create legend and add to map
